1cc0c365fa6a5a39341f240a4eea101f7a3a260c
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-02 23:14:22 +0530

    Social media grid modified

--- src/App.js
+++ src/App.js
@@ -107,6 +107,19 @@ function App() {
 
       <footer>
         <div className="footer-container">
+
+        <div className="social-links">
+            <FaFacebook className="social-icon"/>
+            <FaLinkedin className="social-icon"/>
+            <FaInstagram className="social-icon"/>
+            <div id="social-grid-text">
+              Connect with us on social media!
+            </div>
+            <FaWhatsapp className="social-icon"/>
+            <MdEmail className="social-icon"/>
+            <MdMessage className="social-icon"/>
+          </div>
+
           <form id="contact-form">
             <label htmlFor="name">Name</label>
             <input type="text" id="name" name="name"></input>
@@ -122,15 +135,6 @@ function App() {
             <button className="submit-button">Submit!</button>
           </form>
 
-          <div className="social-links">
-            <FaFacebook />
-            <FaLinkedin />
-            <FaInstagram />
-            <FaWhatsapp />
-            <MdEmail />
-            <MdMessage />
-          </div>
-          
         </div>
       </footer>
     </div>
